61524329cd9b45cc78b6c0e890334e5a32a75308
Wangchi-Acu <wangchi0723@163.com>
2025-07-22 12:28:23 +0800

    Update 2.ISI.py

--- pages/2.ISI.py
+++ pages/2.ISI.py
@@ -17,6 +17,7 @@ def save_csv_isi(name, record):
     
 # ---------- Streamlit 界面 ----------
 st.set_page_config(page_title="失眠严重指数量表 （ISI）", layout="centered")
+st.image("jsszyylogo.png", width=500)  # 更改url_to_your_logo.png为你的logo图片链接，调整width为适当的大小
 st.title("江苏省中医院针灸科失眠专病门诊")
 st.markdown(
     "<h3 style='color:#555555;'>失眠严重指数量表（ISI）在线问卷</h3>",
