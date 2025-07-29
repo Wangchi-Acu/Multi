9d4976ca83b519ada1f8f52ee06934253148cdbb
Wangchi-Acu <wangchi0723@163.com>
2025-07-29 14:57:21 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
                 <div class="sub-title">测评系统</div>
                 <div class="guide">
                     <span class="arrow">👆👆👆</span><br/>
-                    点击左上角 <b>“>>”</b> 展开侧边栏
+                    点击左上角 <b>&gt;&gt;</b> 展开侧边栏<br/>
                     按照顺序开始测评吧~
                 </div>
             </div>
